0b7be4462ab1aafa6cf83969ee4963365f7d4350
Matthew <29123753+silvervulpus@users.noreply.github.com>
2017-11-28 14:42:38 -0600

    Update pythorgorian.py

--- pythorgorian.py
+++ pythorgorian.py
@@ -9,3 +9,11 @@ b = float(input("Please input a number for B squared "))
 content = Pythagoras(a,b)
 content = sqrt(content)
 print ( "The Hypotenuse is  ", content)
+
+def Pythagorian (p,c):
+    theory = (sqrt((c * c) - (p * p)))
+    return theory
+c = float(input("please input the length of side C "))
+p = float(input("Please input the length of side A "))
+contents = Pythagorian(p,c)
+print ("Side B is equal to ", contents)
