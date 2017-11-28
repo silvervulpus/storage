cec35031c03a33ee7d78d7663825e7e860920341
Matthew <29123753+silvervulpus@users.noreply.github.com>
2017-11-28 15:41:58 -0600

    Add files via upload

---
+++ keywordlist.py
@@ -0,0 +1,16 @@
+import random
+rando_Ragne = random.randrange(1,10)
+rando_Nit = random.randint(1,10)
+rando_Loaft = float(rando_Nit)
+rando_Slit = [rando_Ragne, rando_Nit, rando_Loaft]
+randoMunber = random.choice(rando_Slit)
+print (randoMunber)
+print (rando_Ragne)
+print (rando_Loaft)
+print (rando_Nit)
+
+
+""" Key words cannot be used as variable names in python.
+so a good way around this is to play text twist with the key words,
+here are a few examples of how you can twist the keywords to be used
+as easy to remember variable names """
