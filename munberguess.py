ba7a3b490b10d4a5a4a1aac8db7ee57c2d4043f9
Matthew <29123753+silvervulpus@users.noreply.github.com>
2017-12-15 16:11:56 -0600

    Update munberguess.py
    added a choice to play as many times as you wish in a row. line 2 and 3.

--- munberguess.py
+++ munberguess.py
@@ -1,14 +1,16 @@
 import random
-compMunber = random.randint(1,101)
-print("Can you guess what number i am thinking of? You may have 10 guesses.")
-for i in range(10):
-    playerInput = int(input("Please enter the number you are guessing "))
-    if playerInput == compMunber:
-        print("That is correct you win!")
-        break
-    elif playerInput > compMunber:
-        print ("Your Guess is too High!")
-    elif playerInput < compMunber:
-        print ("Your Guess is too Low")
-    else:
-        print("Invalid selection ")
+turns = int(input("Please choose the number of times you would like to play: "))
+for p in range(turns):
+    compMunber = random.randint(1,101)
+    print("Can you guess what number i am thinking of? You may have 10 guesses.")
+    for i in range(10):
+        playerInput = int(input("Please enter the number you are guessing "))
+        if playerInput == compMunber:
+            print("That is correct you win!")
+            break
+        elif playerInput > compMunber:
+            print ("Your Guess is too High!")
+        elif playerInput < compMunber:
+            print ("Your Guess is too Low")
+        else:
+            print("Invalid selection ")
