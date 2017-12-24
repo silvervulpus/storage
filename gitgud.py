9280cb897a98c6905477ac6ec0578c59967ca9e4
Matthew <29123753+silvervulpus@users.noreply.github.com>
2017-12-24 01:25:49 -0600

    Update gitgud.py

--- gitgud.py
+++ gitgud.py
@@ -2,28 +2,19 @@ file = open("kyloth.txt", "w+")
 git = ["you ", "gotta "]
 gud = ["git ", "gud "]
 yaknow = ["you ", "know "]
-for p in yaknow:
-    file.write(p)
-for i in git:
-    file.write(i)
-for n in gud:
-    file.write(n)
-for n in gud:
-    file.write(n)
-for n in gud:
-    file.write(n)
-for p in yaknow:
-    file.write(p)
-for i in git:
-    file.write(i)
-for n in gud:
-    file.write(n)
-for n in gud:
-    file.write(n)
-for n in gud:
-    file.write(n)
+
+for l in range (2):
+    for p in yaknow:
+        file.write(p)
+    for i in git:
+        file.write(i)
+    for f in range (3):
+        for n in gud:
+            file.write(n)
+        
 file.seek(0)
 content = file.readlines()
 file.seek(0)
 file.close()
 print (content)
+
